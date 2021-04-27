02386d1dba14a214f772ee825abb4fc9a9df699a
gerardambe <gerardambe@yahoo.com>
2021-04-27 11:37:30 +0000

    added checkout form data caching in payment intent

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,5 +7,6 @@ app_name = 'orders'
 
 urlpatterns = [
     path('create', views.create_checkout, name='create_checkout'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
